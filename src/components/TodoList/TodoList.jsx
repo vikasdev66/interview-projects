088e53d23e51cf252ae606ac956e6933338a423d
vikasdev66 <vikasmishra.webdev@gmail.com>
2024-08-28 13:01:10 +0530

    todolist created using localstorage

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import { Input } from "antd";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { FaCheck } from "react-icons/fa";
+import { useTodo } from "./useTodo";
 import "./TodoList.css";
 
 export function TodoList() {
+  const {
+    Form,
+    form,
+    handleSubmit,
+    validateTodo,
+    todo,
+    setTodo,
+    todoList,
+    checkedItems,
+    handleCheck,
+    handleDelete,
+  } = useTodo();
   return (
     <div className="main-container">
-      <div className="container"></div>
+      <div className="container">
+        <h1 className="text-center">Todo List Using Localstorage</h1>
+        <Form onFinish={handleSubmit} form={form}>
+          <Form.Item rules={[{ validator: validateTodo }]} name="todo">
+            <Input
+              type="text"
+              value={todo}
+              onChange={(e) => {
+                setTodo(e.target.value);
+              }}
+              placeholder="Please enter your todo"
+            />
+          </Form.Item>
+        </Form>
+        <div className="flex flex-col gap-2 w-6/12 mt-5">
+          {todoList?.map((todoItem, index) => {
+            return (
+              <div
+                key={index}
+                className="todo-item flex items-center justify-between"
+              >
+                <div
+                  className={
+                    checkedItems?.includes(todoItem) ? "line-through" : ""
+                  }
+                >
+                  {todoItem}
+                </div>
+                <div className="flex gap-4">
+                  <RiDeleteBin5Line
+                    onClick={() => {
+                      handleDelete(todoItem);
+                    }}
+                  />
+                  <FaCheck
+                    onClick={() => {
+                      handleCheck(todoItem);
+                    }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoList/useTodo.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { Form } from "antd";
+
+export const useTodo = () => {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [checkedItems, setCheckedItems] = useState([]);
+  const [form] = Form.useForm();
+  const handleSubmit = (e) => {
+    try {
+      setTodoList((prev) => {
+        console.log(prev);
+        const items = [...prev, todo.trim()];
+        localStorage.setItem("todo", JSON.stringify(items));
+        return items;
+      });
+      console.log(todo, e.todo);
+      form.resetFields();
+      setTodo("");
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+
+  const handleDelete = (todoItem) => {
+    try {
+      const deletedTodo = todoList.filter((item) => item !== todoItem);
+      const deletedChecked = checkedItems.filter((item) => item !== todoItem);
+      setTodoList(deletedTodo);
+      setCheckedItems(deletedChecked);
+      localStorage.setItem("todo", JSON.stringify(deletedTodo));
+      localStorage.setItem("checkedItems", JSON.stringify(deletedChecked));
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+
+  const handleCheck = (todoItem) => {
+    setCheckedItems((prev) => {
+      console.log(prev);
+      const items = [...prev, todoItem];
+      localStorage.setItem("checkedItems", JSON.stringify(items));
+      return items;
+    });
+  };
+
+  const validateTodo = (_, value) => {
+    if (todoList.includes(value)) {
+      return Promise.reject(new Error("This todo already exist"));
+    }
+    if (!value) {
+      return Promise.reject(new Error("Please enter todo"));
+    }
+    return Promise.resolve();
+  };
+
+  useEffect(() => {
+    setTodoList(JSON.parse(localStorage.getItem("todo")));
+    setCheckedItems(JSON.parse(localStorage.getItem("checkedItems")));
+  }, []);
+  return {
+    Form,
+    form,
+    handleSubmit,
+    validateTodo,
+    todo,
+    setTodo,
+    todoList,
+    checkedItems,
+    handleCheck,
+    handleDelete,
+  };
+};
